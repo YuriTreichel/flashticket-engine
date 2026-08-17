055f219cf014dfe393c8b0ee6e49b9965c9245cc
Yuri Fernandes <yuriinfo15@gmail.com>
2026-08-17 19:16:13 -0300

    feat: add models and migrations for events, tiers, reservations and tickets

---
+++ database/migrations/2026_08_17_220651_create_tickets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('ticket_tier_id')->constrained('ticket_tiers')->cascadeOnDelete();
+            $table->uuid('code')->unique();
+            $table->string('status')->default('valid');
+            $table->dateTime('used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
